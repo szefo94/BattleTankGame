05e1b505a5f85ef222c27b367930d00d6b76d217
szefo94 <szemarcin@gmail.com>
2018-03-18 18:48:21 +0100

    Moved log to tank aiming component

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
-
+#include "TankAimingComponent.h"
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
 #include "Tank.generated.h"
@@ -14,6 +14,8 @@ class BATTLETANK_API ATank : public APawn
 public:
 	void AimAt(FVector HitLocation);
 
+protected:
+	UTankAimingComponent * TankAimingComponent = nullptr;
 private:
 	// Sets default values for this pawn's properties
 	ATank();
