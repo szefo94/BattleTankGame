64bc93904edb08ef2985dc6c318e8eda2beac842
szefo94 <szemarcin@gmail.com>
2018-04-09 10:56:12 +0200

    Adding TankTrack class, and logging the controller control value to log

---
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankTrack.h"
+#include "Engine/World.h"
+
+void UTankTrack::SetThrottle(float Throttle)
+{
+	auto Time = GetWorld()->GetTimeSeconds();
+	auto Name = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s tracks throttle: %f"), *Name, Throttle)
+	//TODO clamp actual throttle value so player cant over-drive
+}
